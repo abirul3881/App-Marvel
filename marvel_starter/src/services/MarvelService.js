b287d34bf2af0c95c0b9c2cce4c9de097d0a46d2
Andrei <abirul@mail.ru>
2022-07-29 15:32:39 +0300

    first commit

---
+++ marvel_starter/src/services/MarvelService.js
@@ -0,0 +1,27 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+
+  _apiKey = "apikey=bd4cd795e52b8bcd8ad41c2b179d4e31";
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
